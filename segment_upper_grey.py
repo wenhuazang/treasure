c84e03b62b10e292ad5cb1c514579f0e3335b00a
Thelordofdream <574483817@qq.com>
2016-05-24 23:02:01 +0800

    height adaption

--- segment_upper_grey.py
+++ segment_upper_grey.py
@@ -3,19 +3,21 @@ import ImageFont, ImageDraw, ImageFilter, time
 from PIL import Image
 import os
 import pylab
+from numpy import *
 
 BACKGROUND = 255
 NUM = 4
+adds = 60
 
 def findValley(w, y):
     valley = []
-    num = [37, 58, 80]
+    num = [37 + adds, 58 + adds, 80 + adds]
     for x in range(len(num)):
         for i in range(num[x] - 2, num[x] + 3):
             if y[i + 1] >= y[i] and y[i - 1] >= y[i]:
                 valley.append(i + 1)
                 break
-        if len(valley) != x+1:
+        if len(valley) != x + 1:
             valley.append(num[x])
     return valley
 
@@ -26,17 +28,35 @@ def segment(im):
     for i in range(w):
         a = 0
         for j in range(h):
-            a += (255 - frame[i,j])
+            a += (255 - frame[i, j])
         y.append(a)
     valley = findValley(w,y)
-    valley = valley+[13, 102]
+    valley = valley + [13 + adds, 102 + adds]
     valley.sort()
-    print valley
+    #print valley
 
     boxes = []
+    arr = array(im)
+    shadow = [0] * h
+
     for i in range(NUM):
-        boxes.append([valley[i]-1, 6, valley[i+1]+1, 34])
-        print (boxes[i][2] - boxes[i][0])
+        for k in range(valley[i]-1,valley[i + 1]+1):
+            for j in range(h):
+                if not (arr[j, k] == 255):
+                    shadow[j] += 1
+        start = 0
+        sum = 0
+        for k in range(h-27):
+            temp_sum = 0
+            for j in range(28):
+                temp_sum += shadow[k+j]
+            if temp_sum > sum:
+                sum = temp_sum
+                start = k
+        if start < 0:
+            start = 0
+        boxes.append([valley[i] - 1, start, valley[i + 1] + 1, start + 28])
+        #print (boxes[i][2] - boxes[i][0])
     if not boxes:
         return None
     regions = []
@@ -62,17 +82,19 @@ def plot(img):
     for i in range(w):
         a = 0
         for j in range(h):
-            a += (255 - d[i,j])
+            a += (255 - d[i, j])
         y.append(a)
         x.append(i)
-    pylab.plot(x,y)
+    pylab.plot(x, y)
     pylab.show()
 
 
 if __name__ == "__main__":
     start = time.time()
-    img_root = "upper_part_samples/"
-    img_save = "upper_segment/"
+    img_root = "TiebaChinese/"
+    img_save = "results/"
+    if not os.path.exists(img_save):
+        os.makedirs(img_save)
     images = os.listdir(img_root)
     i = -1
     print "load samples..."
@@ -83,8 +105,12 @@ if __name__ == "__main__":
         i += 1
         img = getframe(img_root + name)
         regions = segment(img)
+        if not len(name.split('.')[0].decode('utf-8')) == 4:
+            sys.exit()
         for region in regions:
-            region.save(img_save+str(4*i+a)+".png")
+            if not os.path.exists(img_save + name.split('.')[0].decode('utf-8')[a]):
+                os.makedirs(img_save + name.split('.')[0].decode('utf-8')[a])
+            region.save(img_save + name.split('.')[0].decode('utf-8')[a] + '/' + str(4*i+a) +".png")
             a += 1
     end = time.time()
     print "elapse: %f" % (end - start)
