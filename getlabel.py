207c0e437d018b0a35a6bae879dee9f28f96285c
Thelordofdream <574483817@qq.com>
2016-06-06 04:27:45 +0800

    smart and auto running

--- getlabel.py
+++ getlabel.py
@@ -8,9 +8,10 @@ from os import listdir
 from PIL import Image
 import numpy as np
 from numpy import *
+import thread
+
 
 class RClient(object):
-
     def __init__(self, username, password, soft_id, soft_key):
         self.username = username
         self.password = md5(password).hexdigest()
@@ -54,30 +55,88 @@ class RClient(object):
         return r.json()
 
 
-if __name__ == '__main__':
-    rc = RClient('username', 'password', '60913', 'e7d4651bbaf148f9b37f8b9307d820d9')
-    files = listdir('Image/')
-    for image_name in files:
-        if image_name == '.DS_Store':
-            continue
-        im = open('Image/' + image_name, 'rb').read()
-        image = Image.open('Image/' + image_name)
-        label = rc.rk_create(im, 4010)['Result']
-        print label
-        Im = array(image)
-        mean = np.mean(Im)
-        column, row = image.size
-        for a in range(row):
-            for b in range(column):
-                if Im[a, b] < (mean - 33):
-                    Im[a, b] = 0
-                else:
-                    Im[a, b] = 255
-        for a in range(35):
-            for b in range(40):
-                pass
-        image = Image.fromarray(Im)
-        if not os.path.exists('DATA/'+ label):
-            os.makedirs('DATA/'+ label)
-        image.save('DATA/'+ label + '/' + image_name)
+def getlabel1(files, stop_point, root1, root2, finished_thread):
+    stop_point0 = stop_point
+    flag = 0
+    present = '-1'
+    while not (present == files[-1].split('.')[0] or present == files[-2].split('.')[0]):
+        for image_name in files:
+            if image_name == '.DS_Store':
+                continue
+            if image_name.split('.')[0] == stop_point0:
+                flag = 1
+            if flag == 1:
+                if int(image_name.split('.')[0]) % 2 == 1:
+                    im = open(root1 + image_name, 'rb').read()
+                    image = Image.open(root1 + image_name)
+                    try:
+                        label = rc.rk_create(im, 4010)['Result']
+                        if not os.path.exists(root2 + label):
+                            os.makedirs(root2 + label)
+                        image.save(root2 + label + '/' + image_name)
+                        present = image_name.split('.')[0]
+                        print present + ':' + label
+                    except:
+                        print image_name.split('.')[0] + ':' + 'connection error!'
+                        stop_point0 = image_name.split('.')[0]
+                        flag = 0
+                        break
+    finished_thread[0] += 1
 
+
+def getlabel2(files, stop_point, root1, root2, finished_thread):
+    stop_point0 = stop_point
+    flag = 0
+    present = '-1'
+    while not (present == files[-1].split('.')[0] or present == files[-2].split('.')[0]):
+        for image_name in files:
+            if image_name == '.DS_Store':
+                continue
+            if image_name.split('.')[0] == stop_point0:
+                flag = 1
+            if flag == 1:
+                if int(image_name.split('.')[0]) % 2 == 0:
+                    im = open(root1 + image_name, 'rb').read()
+                    image = Image.open(root1 + image_name)
+                    try:
+                        label = rc.rk_create(im, 4010)['Result']
+                        present = image_name.split('.')[0]
+                        print present + ':' + label
+                        if not os.path.exists(root2 + label):
+                            os.makedirs(root2 + label)
+                        image.save(root2 + label + '/' + image_name)
+                    except:
+                        print image_name.split('.')[0] + ':' + 'connection error!'
+                        stop_point0 = image_name.split('.')[0]
+                        flag = 0
+                        break
+    finished_thread[0] += 1
+
+
+if __name__ == '__main__':
+    rc = RClient('username', 'passwiord', '60913', 'e7d4651bbaf148f9b37f8b9307d820d9')
+    root1 = 'Image/'
+    root2 = 'DATA/'
+    files = listdir(root1)
+    m = len(files) / 2
+    post_mode = 0  # 0是从头运行的模式;1是人为断点启动模式
+    if post_mode == 0:
+        stop_point1_1 = files[1].split('.')[0]
+        stop_point1_2 = files[1].split('.')[0]
+        stop_point2_1 = files[m].split('.')[0]
+        stop_point2_2 = files[m].split('.')[0]
+    else:
+        stop_point1_1 = '12567'
+        stop_point1_2 = '12568'
+        stop_point2_1 = '12589'
+        stop_point2_2 = '12590'
+    finished_thread = [0]
+    try:
+        thread.start_new_thread(getlabel1, (files[:m], stop_point1_1, root1, root2, finished_thread))
+        thread.start_new_thread(getlabel2, (files[:m], stop_point1_2, root1, root2, finished_thread))
+        thread.start_new_thread(getlabel1, (files[m:], stop_point2_1, root1, root2, finished_thread))
+        thread.start_new_thread(getlabel2, (files[m:], stop_point2_2, root1, root2, finished_thread))
+    except:
+        print "Error: unable to start thread"
+    while finished_thread[0] < 4:
+        pass
